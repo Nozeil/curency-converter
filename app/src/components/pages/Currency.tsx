a99ad6c9eb1f5b031a994679e6474239ab01e8ec
Artur Voloshin <nozeilwrk@gmail.com>
2023-03-29 20:56:23 +0300

    feat: implement currency select component

--- app/src/components/pages/Currency.tsx
+++ app/src/components/pages/Currency.tsx
@@ -1,3 +1,31 @@
+import { SUPPORTED_CURRENCIES } from '@/main.constants';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
+
+const LABEL = 'Currency';
+
+function CurrencySelect() {
+  return (
+    <TextField
+      id="outlined-select-currency"
+      select
+      margin="normal"
+      label={LABEL}
+      defaultValue="BYN"
+    >
+      {SUPPORTED_CURRENCIES.map((item) => (
+        <MenuItem key={item.code} value={item.code}>
+          {`${item.code}, ${item.name}, ${item.country}`}
+        </MenuItem>
+      ))}
+    </TextField>
+  );
+}
+
 export function Currency() {
-  return <div>Currency</div>;
+  return (
+    <section>
+      <CurrencySelect />
+    </section>
+  );
 }
